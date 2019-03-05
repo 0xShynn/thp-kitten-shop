3617f06168a3db77d7094ae8b00ca32eef4a3db4
Laurane01 <petitlaurane01@gmail.com>
2019-03-05 11:48:48 +0100

    added active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_03_05_085023) do
-=======
-ActiveRecord::Schema.define(version: 2019_03_05_100437) do
->>>>>>> delivery-laurane-active-storage
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
