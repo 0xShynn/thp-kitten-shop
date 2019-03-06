c9f2b926e45c2c8f6cdb1eed2b9523e31f8b4680
anhek <shynn@icloud.com>
2019-03-06 15:14:02 +0100

    user.rb fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,32 +7,23 @@ class User < ApplicationRecord
   has_one :cart
   has_many :orders
   has_many :items, through: :orders
+  has_one_attached :avatar
 
-  after_create :welcome_send
   before_create :default_values
-
-
-
-
+  after_create :welcome_send
+ 
+  
   # Création du cart associé à l'utilisateur en même temps que l'inscription
   after_create do 
     id = User.all.last.id
     Cart.create!(user_id: id)
   end
 
-<<<<<<< HEAD
-  has_many :orders
-  has_many :items, through: :orders
-  has_one_attached :avatar
-
-  after_create :welcome_send
-=======
->>>>>>> e1bfdae8e1ab4f2aaa7cd9bdb118a875ee5cde78
-  
-private
   def welcome_send
     UserMailer.welcome_email(self).deliver_now!
   end
+
+  private
 
   def default_values
     self.is_admin ||= false
