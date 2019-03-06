e59985101218f726c3a7a39daec442419f41fbee
Mallauy <mallaury.bajas1@gmail.com>
2019-03-06 12:26:28 +0100

    edit and show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
     puts '$' * 60
     puts @orders
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+     @user = current_user
+      if @user.update_attributes(user_params)
+        redirect_to user_path
+    else
+      render 'edit'
+    end
+  end 
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
 end
