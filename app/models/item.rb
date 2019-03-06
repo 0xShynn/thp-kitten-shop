7b55ae48b013ae63b478546f92abb299d3c992e7
degregor69 <raphael.degregorio@gmail.com>
2019-03-06 16:14:47 +0100

    ok

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
     has_many :orders
     has_one_attached :photo
 
-    def thumbnail_300
-        return self.photo.variant(resize: '300x300')
-    end
+    # def thumbnail_300
+    #     return self.photo.variant(resize: '300x300')
+    # end
 end
