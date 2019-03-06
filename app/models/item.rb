0eeff0c279eff2183dd9c227997e78a7dd520363
anhek <shynn@icloud.com>
2019-03-06 16:42:56 +0100

    cleaning files

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
     has_one_attached :photo
 
     def thumbnail_300
+      if self.photo == true
         return self.photo.variant(resize: '300x300')
+      end
     end
 end
