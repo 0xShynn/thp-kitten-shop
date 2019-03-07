101782b40512a81f909fd6d903ea4c714ce633fb
Mallauy <mallaury.bajas1@gmail.com>
2019-03-07 16:34:04 +0100

    ok

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
+    first_name { Faker::Superhero.prefix }
+    last_name { Faker::BackToTheFuture.character }
+    email { Faker::Internet.email }
+    password { Faker::Ethereum.address }
     
+
   end
 end
